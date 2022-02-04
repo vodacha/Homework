5c7c0561017afa03f633471bcf5a22d5e2c6f040
Andrey Kolev <andrei_kolev1@abv.bg>
2022-02-04 12:44:20 +0200

    Initial commit

---
+++ src/main/java/Java/Homework/Homework_Java.java
@@ -0,0 +1,18 @@
+package Java.Homework;
+
+public class Homework_Java {
+    public static void main(String[] args) {
+        String vehicleString = "Audi S8";
+        String vehicle1String = "BMW 760 Li";
+
+        System.out.println("My favorite car is: " + vehicle1String);
+
+        System.out.println(vehicleString.equals(vehicle1String));
+
+        System.out.println(vehicleString.length());
+        System.out.println(vehicle1String.length());
+
+        System.out.println(vehicleString + vehicle1String);
+
+        }
+    }
